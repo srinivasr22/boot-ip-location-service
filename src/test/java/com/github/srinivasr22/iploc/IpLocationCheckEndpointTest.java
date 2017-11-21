7e3464aed44ba0a2b1c6558aa960a740a90cb28e
srinivasr22 <srinivasr22@gmail.com>
2017-11-20 21:07:00 -0800

    Adding MockMvc test

--- src/test/java/com/github/srinivasr22/iploc/IpLocationCheckEndpointTest.java
+++ src/test/java/com/github/srinivasr22/iploc/IpLocationCheckEndpointTest.java
@@ -1,15 +1,40 @@
 package com.github.srinivasr22.iploc;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.github.srinivasr22.iploc.controller.IpLocationCheckEndpoint;
+import com.github.srinivasr22.iploc.service.ExternalLocationCheckService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IpLocationCheckEndpointTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Before
 	public void setUp() throws Exception {
+
 	}
 
 	@After
@@ -17,8 +42,10 @@ public class IpLocationCheckEndpointTest {
 	}
 
 	@Test
-	public void testCheckIfUSIp() {
-		fail("Not yet implemented");
+	public void testCheckIfUSIp() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/check/us?ip=1.1.1.1"))
+					.andDo(MockMvcResultHandlers.print())
+					.andExpect(MockMvcResultMatchers.status().isOk())
+					.andExpect(MockMvcResultMatchers.content().string("false"));
 	}
-
 }
