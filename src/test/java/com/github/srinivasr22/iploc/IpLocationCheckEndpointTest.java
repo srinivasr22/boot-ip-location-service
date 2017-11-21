9975b302f5559fa31272dc6b6095e6b7d4cbad13
srinivasr22 <srinivasr22@gmail.com>
2017-11-20 21:08:09 -0800

    Removing unnecessary imports

--- src/test/java/com/github/srinivasr22/iploc/IpLocationCheckEndpointTest.java
+++ src/test/java/com/github/srinivasr22/iploc/IpLocationCheckEndpointTest.java
@@ -1,28 +1,17 @@
 package com.github.srinivasr22.iploc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.github.srinivasr22.iploc.controller.IpLocationCheckEndpoint;
-import com.github.srinivasr22.iploc.service.ExternalLocationCheckService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
